8a9ff08379914a7d08671e802df54e132f09314f
Fred Chien <cfsghost@gmail.com>
2015-08-21 07:38:37 +0800

    fixed a bug that cannot found `window` variable

--- src/fluky.js
+++ src/fluky.js
@@ -3,7 +3,7 @@ var Core = require('./core');
 var core = new Core();
 
 // Load default state on browser-side
-if (window) {
+if (typeof window != 'undefined') {
 	if (window.Fluky) {
 		if (window.Fluky.state) {
 			core.setInitialState(window.Fluky.state);
